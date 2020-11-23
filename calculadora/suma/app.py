dd8f8c85716eaa1a24a315ee20c81884261dc5ea
efrenabde <51309699+efrenabde@users.noreply.github.com>
2020-11-23 17:12:11 -0500

    Ajuste calculadora
    Ajuste calculadora

--- calculadora/suma/app.py
+++ calculadora/suma/app.py
@@ -5,5 +5,6 @@ app =Flask(__name__)
 @app.route("/suma/<int:value1>/<int:value2>")
 def suma(value1, value2):
     return str(value1+value2)
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000,debug=True)
